936c57e747523ec7c41479d97290ad79582eae25
Károly Füzessi <karoly.fuzessi@presight.com>
2022-03-26 22:34:52 +0100

    data name and iife; result file

---
+++ count-data.ts
@@ -0,0 +1,31 @@
+(() => {
+  const folder = "./data/";
+  const fs = require("fs");
+  const resultFileName = "result-data.csv";
+
+  fs.writeFileSync(resultFileName, "");
+
+  fs.readdirSync(folder).map((file: string, index: number) => {
+    const data = fs.readFileSync(`${folder}/${file}`, "utf8");
+    const lines = data.split("\n");
+    const results = [] as string[][];
+    lines
+      .filter((line: string) => line !== "")
+      .map((line: string, index: number) =>
+        results.push([
+          file,
+          String(index),
+          line.split(" ")[0],
+          String(
+            line
+              .split(" ")
+              .filter(
+                (e: string, i: number) =>
+                  i > 0 && !["?", "'", '"', "...", "*", "-", "#"].includes(e)
+              ).length
+          ),
+        ])
+      );
+    fs.appendFileSync(resultFileName, results.join("\n"));
+  });
+})();
